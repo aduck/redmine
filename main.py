f62a3cba10cf826b9e1c20f4637d6d54f7d2c220
hyy <hanyaya@benlai.com>
2018-03-29 19:18:40 +0800

    fixed errer

--- main.py
+++ main.py
@@ -14,14 +14,14 @@ if __name__ == '__main__':
     for item in lists:
         issueId = item['issueId']
         date = getDtl(issueId)
-        item['startDate'] = date[0]
-        item['endDate'] = date[1]
         # 开始日期
-        startDate = date[0]
+        startDate = date[0] or '1990-01-01'
         # 结束日期
-        endDate = date[1] or 0
+        endDate = date[1] or '2099-12-31'
+        item['startDate'] = startDate
+        item['endDate'] = endDate
         # 已完成的issues
-        if (formatDate(endDate) <= formatDate(DATE[1]) and formatDate(endDate) >= formatDate(DATE[0])) or (item['issueStatus'] == '提测' and formatDate(endDate) >= formatDate(DATE[0])):
+        if (formatDate(endDate) <= formatDate(DATE[1]) and formatDate(endDate) >= formatDate(DATE[0])) or (item['issueStatus'] in ['提测', '测试中', 'merge完成', '已关闭'] and formatDate(endDate) >= formatDate(DATE[0])):
             doneLists.append(item)
         # 未开始的列表
         elif formatDate(startDate) > formatDate(DATE[1]):
--- md.py
+++ md.py
@@ -11,21 +11,21 @@ def create(doneLists = [], doLists = [], todoLists = []):
     str += '| :-: | - | :-: | :-: | :-: | - |\r'
     # 已完成
     for index, doneItem in enumerate(doneLists):
-        str += '| %s | %s | 100 | %s | %s | %s |\r' %((index + 1), doneItem['issueName'], doneItem['startDate'], doneItem['endDate'], doneItem['issueStatus'])
+        str += '| %s | %s | 100 | %s | %s | %s |\r' %((index + 1), doneItem['issueName'], doneItem['startDate'] if doneItem['startDate'] != '1990-01-01' else '', doneItem['endDate'] if doneItem['endDate'] != '2099-12-31' else '', doneItem['issueStatus'])
     # 进行中
     for index, doItem in enumerate(doLists):
-        # 算百分比
-        per = (formatDate(DATE[1]) + 86400.0 - formatDate(doItem['startDate'])) / (formatDate(doItem['endDate']) - formatDate(doItem['startDate'])) * 100
+        # 算百分比（一般情况需要减去两天周末）
+        per = (formatDate(DATE[1]) + 86400.0 - formatDate(doItem['startDate'])) / (formatDate(doItem['endDate']) - formatDate(doItem['startDate']) - 86400.0 * 2) * 100
         # 备注
         note = per > 50 and '前端页面完成需要和api联调' or '前端功能待优化和与api联调'
-        str += '| %s | %s | %d | %s | %s | %s |\r' %((index + 1 + doneLen), doItem['issueName'], per, doItem['startDate'], doItem['endDate'], note)
+        str += '| %s | %s | %d | %s | %s | %s |\r' %((index + 1 + doneLen), doItem['issueName'], per, doItem['startDate'] if doItem['startDate'] != '1990-01-01' else '', doItem['endDate'] if doItem['endDate'] != '2099-12-31' else '', note)
     # 未开始
     str += '\r\n'
     str += '### 下周工作计划\r'
     str += '|   | 工作内容 | 工作进度 | 开始时间 | 计划完成时间 | 备注 |\r'
     str += '| :-: | - | :-: | :-: | :-: | - |\r'
     for index, todoItem in enumerate(todoLists):
-        str += '| %s | %s |  | %s | %s |  |\r' %((index + 1), todoItem['issueName'], todoItem['startDate'], todoItem['endDate'])
+        str += '| %s | %s |  | %s | %s | \000 |\r' %((index + 1), todoItem['issueName'], todoItem['startDate'] if todoItem['startDate'] != '1990-01-01' else '', todoItem['endDate'] if todoItem['endDate'] != '2099-12-31' else '')
     str += '\r\n'
     with open('任务.md', 'w', encoding = 'utf-8') as f:
         f.write(str)
